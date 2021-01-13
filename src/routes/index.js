78ba157f8ed93a338605d095d2d5ce56175c0436
Jose Morris-Garay <joseg707@gmail.com>
2021-01-13 10:38:10 -0800

    Initial Commit.

---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+import express from 'express'
+const router = express.Router()
+router.get('/', (req, res, next) =>
+    res.status(200).json({ message: 'Welcome to Express API template' })
+)
+export default router
